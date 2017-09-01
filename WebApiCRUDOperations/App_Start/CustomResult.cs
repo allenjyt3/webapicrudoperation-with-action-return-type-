f48f453d3e4210a70d382bca00410e7026e3b19b
ajasseessi <ajasseessi@SUYPC214>
2017-09-01 15:30:10 +0530

    Add project files.

---
+++ WebApiCRUDOperations/App_Start/CustomResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApiCRUDOperations.App_Start
+{
+    public class CustomResult : IHttpActionResult
+    {
+        string _value;
+        string _value2;
+        HttpRequestMessage _request;
+        HttpStatusCode _code;
+
+
+        public CustomResult(string value,string value2,HttpRequestMessage request,HttpStatusCode code)
+        {
+            _value = value;
+            _value2 = value2;
+            _request = request;
+            _code = code;
+        }
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage()
+            {
+                Content = new StringContent(_value +" "+_value2), 
+                RequestMessage = _request,
+                StatusCode = _code
+            };
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ WebApiCRUDOperations/Controllers/StudController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiCRUDOperations.App_Start;
+using WebApiCRUDOperations.Models;
+
+namespace WebApiCRUDOperations.Controllers
+{
+    public class StudController : ApiController
+    {
+        [HttpGet]
+        [Route("findStudent")]
+        public HttpResponseMessage Get(int id)
+        {
+            Student student = new Student();
+            using (var context = new SchoolEntities())
+            {
+                student = context.Students.Where(s => s.StudentId == id).FirstOrDefault();
+            }
+            if (student == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, id);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, student);
+        }
+
+
+        [Route("CheckStudent")]
+        public IHttpActionResult GetStudent(int id)
+        {
+            Student student = new Student();
+            using (var context = new SchoolEntities())
+            {
+                student = context.Students.Where(s => s.StudentId == id).FirstOrDefault();
+            }
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+
+        private HttpResponseMessage OK(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("CustomStudent")]
+        public IHttpActionResult GetStudentUsingCustom(int id)
+        {
+            Student student = new Student();
+            using (var context = new SchoolEntities())
+            {
+                student = context.Students.Where(s => s.StudentId == id).FirstOrDefault();
+            }
+            if (student == null)
+            {
+                return new App_Start.CustomResult("NOT FOUND", "NOT FOUND", Request, HttpStatusCode.NotFound);
+            }
+            return new CustomResult(student.FirstName,student.LastName, Request, HttpStatusCode.Found);
+        }
+    }
+}
